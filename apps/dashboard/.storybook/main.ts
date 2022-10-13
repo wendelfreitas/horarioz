5b8d53d8d2ac6ebb49c2918890c6ce6340f379e0
Wendel Freitas <wendelfreitasbranco@gmail.com>
2022-10-12 23:53:21 -0300

    feat: add jest and storybook to dashboard project

---
+++ apps/dashboard/.storybook/main.ts
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
+    'storybook-addon-react-docgen',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  features: {
+    storyStoreV7: true,
+  },
+};
---
+++ apps/dashboard/jest.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/*.stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts(x)?',
+    '!src/stories/**',
+    '!src/main.tsx',
+    '!src/index.css',
+    '!src/App.tsx',
+    '!src/vite-env.d.ts',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};

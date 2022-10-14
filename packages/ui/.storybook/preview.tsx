3be388b02fd4859d10bf7866a12154d619dfb6fc
Bruno Gustavo <brugucolosil95@gmail.com>
2022-10-13 23:36:30 -0300

    chore(fonts): change location of global.css

--- packages/ui/.storybook/preview.tsx
+++ packages/ui/.storybook/preview.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from '../src/styles/ThemeProvider/ThemeProvider';
-import './global.css';
+import '../src/styles/global.css';
 
 // Global decorator to apply the styles to all stories
 export const decorators = [

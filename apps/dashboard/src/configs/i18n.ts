165d8074e037a4a9a0106fd50b4e53baf0c3903e
Wendel Freitas <wendelfreitasbranco@gmail.com>
2023-05-07 03:06:23 -0300

    chore: add english language for locales

--- apps/dashboard/src/configs/i18n.ts
+++ apps/dashboard/src/configs/i18n.ts
@@ -1,6 +1,7 @@
 import { i18n } from '@horarioz/i18n';
 
 import ptbr from '../locales/ptbr/translation.json';
+import en from '../locales/en/translation.json';
 import { locales } from '@horarioz/supabase';
 
 const resources = {
@@ -10,13 +11,18 @@ const resources = {
       ...locales.ptbr,
     },
   },
+  en: {
+    translation: {
+      ...en,
+    },
+  },
 };
 
 i18n.init({
   returnNull: false,
   resources,
   lng: 'ptbr',
-  fallbackLng: 'ptbr',
+  fallbackLng: 'en',
   interpolation: {
     escapeValue: false,
   },

e16eb591c8d6ca906db7b042b2e1eddadd4de4eb
Wendel Freitas <wendelfreitasbranco@gmail.com>
2023-05-08 00:30:59 -0300

    feat: create select component with two types, autocomplete and listbox

---
+++ packages/ui/src/components/Select/Select.test.tsx
@@ -0,0 +1,127 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { Select } from './Select';
+
+const pokemons = [
+  { value: '1', label: 'Blaziken' },
+  { value: '2', label: 'Tyranitar' },
+  { value: '3', label: 'Raichu' },
+  { value: '4', label: 'Salamance' },
+  { value: '5', label: 'Alakazam' },
+  { value: '6', label: 'Mewtwo (Are you kidding me right?)', disabled: true },
+];
+
+describe('<Select />', () => {
+  it('should render component successfully', () => {
+    render(
+      <Select
+        type="autocomplete"
+        label="Select pokemons"
+        placeholder="Select the best pokemon"
+        defaultValue="1"
+        options={pokemons}
+      />
+    );
+    const text = screen.getByText('Select pokemons');
+
+    expect(text).toBeInTheDocument();
+  });
+
+  it('should type a text and find the specific option', () => {
+    render(
+      <Select
+        type="autocomplete"
+        label="Select pokemons"
+        placeholder="Select the best pokemon"
+        defaultValue="1"
+        options={pokemons}
+      />
+    );
+    const input = screen.getByRole('combobox');
+    fireEvent.change(input, { target: { value: 'Tyranitar' } });
+
+    expect(screen.getByText('Tyranitar')).toBeInTheDocument();
+  });
+
+  it('should select an option on listbox', () => {
+    render(
+      <Select
+        label="Select pokemons"
+        placeholder="Select the best pokemon"
+        defaultValue="1"
+        options={pokemons}
+      />
+    );
+    const input = screen.getByRole('combobox');
+    fireEvent.change(input, { target: { value: 'Tyranitar' } });
+
+    expect(screen.getByText('Tyranitar')).toBeInTheDocument();
+  });
+
+  it('should render an helper text', () => {
+    render(
+      <Select
+        label="Select pokemons"
+        placeholder="Select the best pokemon"
+        defaultValue="1"
+        helper="Helper text"
+        options={pokemons}
+      />
+    );
+
+    expect(screen.getByText('Helper text')).toBeInTheDocument();
+  });
+
+  it('should type a text and no find any option', () => {
+    render(
+      <Select
+        type="autocomplete"
+        label="Select pokemons"
+        placeholder="Select the best pokemon"
+        defaultValue="1"
+        options={pokemons}
+      />
+    );
+
+    const input = screen.getByRole('combobox');
+    fireEvent.change(input, {
+      target: { value: 'THE BEST POKEMON IS BLAZIKEN ALWAYS' },
+    });
+
+    expect(screen.getByText('No options.')).toBeInTheDocument();
+  });
+
+  it('should render a list of options', () => {
+    render(
+      <Select
+        type="autocomplete"
+        label="Select pokemons"
+        placeholder="Select the best pokemon"
+        options={pokemons}
+      />
+    );
+
+    const input = screen.getByRole('combobox');
+    fireEvent.change(input, {
+      target: { value: 'A' },
+    });
+
+    expect(screen.getByText('Salamance')).toBeInTheDocument();
+    expect(screen.getByText('Alakazam')).toBeInTheDocument();
+  });
+
+  it('should render an * if required is true', async () => {
+    render(
+      <Select
+        label="Select pokemons"
+        type="autocomplete"
+        placeholder="Select the best pokemon"
+        required
+        options={pokemons}
+      />
+    );
+
+    expect(screen.getByText('*')).toBeInTheDocument();
+  });
+});
---
+++ packages/ui/src/components/Select/Select.tsx
@@ -0,0 +1,213 @@
+import { Fragment, InputHTMLAttributes, useState } from 'react';
+import { Combobox, Transition } from '@headlessui/react';
+import { getInputClasses, getLabelClasses } from '../Input/Input';
+import { ChevronDownIcon } from '@radix-ui/react-icons';
+import cn from 'classnames';
+import uniqid from 'uniqid';
+
+type Option = {
+  label: string;
+  value: string;
+  disabled?: boolean;
+};
+
+const Type = {
+  AUTOCOMPLETE: 'autocomplete',
+  LISTBOX: 'listbox',
+};
+
+export type SelectProps = {
+  /**
+   * The type of the select component, if the type is autocomplete the select will be a combobox with an input to filter results, otherwise will be a listbox.
+   */
+  type?: (typeof Type)[keyof typeof Type];
+  /**
+   * Add a text with color red to the input
+   */
+  error?: string;
+  /**
+   * Add a helper text below the input.
+   */
+  helper?: string;
+  /**
+   * The input label, if the label and placeholder have same value this label will be a floating label.
+   */
+  label?: string;
+  /**
+   * If the select is disabled.
+   */
+  disabled?: boolean;
+  /**
+   * If the select is required.
+   */
+  required?: boolean;
+  /**
+   * The list of combobox options
+   */
+  options: Option[];
+  /**
+   * Label to show when options is empty.
+   */
+  emptyLabel?: string;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+export const Select = ({
+  label,
+  options,
+  type = Type.LISTBOX,
+  emptyLabel = 'No options.',
+  ...props
+}: SelectProps) => {
+  const id = props.id || uniqid('select_');
+  const isAutocomplete = type === Type.AUTOCOMPLETE;
+  const [query, setQuery] = useState('');
+
+  const getOptionsListClasses = () => {
+    return cn(
+      'border',
+      'border-black',
+      'absolute',
+      'mt-2',
+      'max-h-60',
+      'w-full',
+      'overflow-auto',
+      'rounded-md',
+      'bg-white',
+      'py-0',
+      'text-base',
+      'shadow',
+      'ring-1',
+      'ring-black',
+      'ring-opacity-5',
+      'focus:outline-none',
+      'sm:text-sm'
+    );
+  };
+
+  const getResults = () => {
+    if (!query.trim()) {
+      return options;
+    }
+
+    return options.filter((option) =>
+      option.label
+        .toLowerCase()
+        .replace(/\s+/g, '')
+        .includes(query.toLowerCase().replace(/\s+/g, ''))
+    );
+  };
+
+  const defaultValue = options.find(
+    (option) => option.value === props.defaultValue
+  );
+
+  const Messages = () => (
+    <Fragment>
+      <Transition
+        show={Boolean(props.error)}
+        enter="transition-opacity duration-500"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <legend className={cn('text-red-500', 'text-xs', 'mt-2', 'ml-2')}>
+          {props.error}
+        </legend>
+      </Transition>
+      {props.helper && (
+        <legend className={cn('text-xs', 'mt-2', 'ml-2')}>
+          {props.helper}
+        </legend>
+      )}
+    </Fragment>
+  );
+
+  return (
+    <Combobox name={props.name} defaultValue={defaultValue}>
+      {({ open }) => (
+        <Fragment>
+          <div className="relative mt-1">
+            <div className="relative">
+              <Combobox.Button
+                className={cn('w-full', {
+                  'cursor-pointer': !isAutocomplete,
+                })}
+              >
+                <Combobox.Input
+                  id={id}
+                  contentEditable={isAutocomplete}
+                  readOnly={!isAutocomplete || !open}
+                  displayValue={(option: Option) => option?.label}
+                  onChange={(event) => setQuery(event.target.value)}
+                  className={cn(
+                    getInputClasses(props),
+                    {
+                      '!border-black': open,
+                    },
+                    {
+                      'cursor-pointer': !isAutocomplete,
+                    }
+                  )}
+                  placeholder={props.placeholder}
+                />
+              </Combobox.Button>
+              <label htmlFor={id} className={getLabelClasses(props)}>
+                {label}{' '}
+                {props.required && <span className="text-red-500">*</span>}
+              </label>
+              <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                <ChevronDownIcon
+                  className="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </Combobox.Button>
+            </div>
+            <Combobox.Options className={getOptionsListClasses()}>
+              {!getResults().length ? (
+                <div className="relative cursor-default select-none py-2 px-4 text-gray-400">
+                  {emptyLabel}
+                </div>
+              ) : (
+                getResults().map((option) => (
+                  <Combobox.Option
+                    key={option.value}
+                    value={option}
+                    disabled={option.disabled}
+                    className={({ active, disabled }) =>
+                      cn(
+                        'relative',
+                        'cursor-default',
+                        'select-none',
+                        'py-2',
+                        'pl-4',
+                        'pr-4',
+                        'text-gray-500',
+                        {
+                          'bg-gray-50 text-black hover:cursor-pointer': active,
+                        },
+                        { 'text-gray-300 hover:cursor-not-allowed': disabled }
+                      )
+                    }
+                  >
+                    {({ selected }) => (
+                      <span
+                        className={cn('block', 'truncate', {
+                          'text-black': selected,
+                        })}
+                      >
+                        {option.label}
+                      </span>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </div>
+          <Messages />
+        </Fragment>
+      )}
+    </Combobox>
+  );
+};

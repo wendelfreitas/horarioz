93749342a1224236095e3d3e64ceda790c679f6b
Bruno Gustavo <brugucolosil95@gmail.com>
2023-05-20 15:32:21 -0300

    refactor(rootImport): simple way to configure vite

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, '/src/components'),
-      '@utils': path.resolve(__dirname, '/src/utils'),
-      '@locales': path.resolve(__dirname, '/src/locales'),
-      '@assets': path.resolve(__dirname, '/src/assets'),
+      '@': './src',
+      '@components': '/src/components',
+      '@utils': '/src/utils',
+      '@locales': '/src/locales',
+      '@assets': '/src/assets',
     },
   },
 });

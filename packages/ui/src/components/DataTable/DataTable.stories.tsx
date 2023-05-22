699f8afd561d924294d9f56b8740906b039ea243
Wendel Freitas <wendelfreitasbranco@gmail.com>
2023-05-22 19:14:30 -0300

    feat: create datable component

---
+++ packages/ui/src/components/DataTable/DataTable.stories.tsx
@@ -0,0 +1,54 @@
+import { StoryFn, Meta } from '@storybook/react';
+import { ColumnDef } from '@tanstack/react-table';
+import { DataTable } from './DataTable';
+
+export default {
+  title: 'DataTable',
+  component: DataTable,
+} as Meta;
+
+type Payment = {
+  id: string;
+  amount: number;
+  status: 'pending' | 'processing' | 'success' | 'failed';
+  email: string;
+};
+
+const payments: Payment[] = [
+  {
+    id: '728ed52f',
+    amount: 100,
+    status: 'pending',
+    email: 'm@example.com',
+  },
+  {
+    id: '489e1d42',
+    amount: 125,
+    status: 'processing',
+    email: 'example@gmail.com',
+  },
+  // ...
+];
+
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'status',
+    header: 'Status',
+  },
+  {
+    accessorKey: 'email',
+    header: 'Email',
+  },
+  {
+    accessorKey: 'amount',
+    header: 'Amount',
+  },
+];
+
+export const Default: StoryFn = () => (
+  <DataTable data={payments} columns={columns} />
+);
+
+export const WithNoData: StoryFn = () => (
+  <DataTable data={[]} columns={columns} />
+);
---
+++ packages/ui/src/components/DataTable/DataTable.tsx
@@ -0,0 +1,92 @@
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '../Table/Table';
+
+export type DataTableProps<TData, TValue> = {
+  /**
+   * An array of ColumnDef objects defining the table columns and how to render their contents.
+   */
+  columns: ColumnDef<TData, TValue>[];
+  /**
+   *  An array of objects representing the data records to be displayed in the table.
+   */
+  data: TData[];
+  /**
+   * A custom ReactNode or string to be displayed when the table is empty. Optional.
+   */
+  empty?: React.ReactNode | string;
+};
+
+/**
+ * Datable component to render table data for users
+ */
+export const DataTable = <TData, TValue>({
+  columns,
+  data,
+  empty = 'No items',
+}: DataTableProps<TData, TValue>) => {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && 'selected'}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                {empty}
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};

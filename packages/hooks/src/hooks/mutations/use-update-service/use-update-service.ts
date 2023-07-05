6989944c32ea0501cc11b45d7e7495325f94f984
Wendel Freitas <wendelfreitasbranco@gmail.com>
2023-07-05 17:49:11 -0300

    feat: add crud service hooks

--- packages/hooks/src/hooks/mutations/use-update-service/use-update-service.ts
+++ packages/hooks/src/hooks/mutations/use-update-service/use-update-service.ts
@@ -4,35 +4,39 @@ import { Database } from '@horarioz/supabase';
 import { PostgrestError } from '@supabase/supabase-js';
 
 type ServiceInput = {
-  company_id: string;
   name: string;
-  description?: string;
+  description: string;
   price: number;
   duration: string;
 };
 
-type CreateServiceResponse = {
-  data: Database['public']['Tables']['services']['Row'] | null;
+type Service = Database['public']['Tables']['services']['Row'];
+
+type UpdateServiceResponse = {
+  data: Service | null;
   error: PostgrestError | null;
 };
 
-export const useCreateService = (
-  options?: MutateOptions<CreateServiceResponse, Error, ServiceInput>
+type Filter = {
+  column: keyof Pick<Service, 'id'>;
+  value?: string;
+};
+
+export const useUpdateService = (
+  filter: Filter = {
+    column: 'id',
+  },
+  options?: MutateOptions<UpdateServiceResponse, Error, ServiceInput>
 ) => {
   const supabase = useSupabase();
 
   return useMutation(
-    ['create', 'service'],
-    async ({
-      company_id,
-      name,
-      description,
-      price,
-      duration,
-    }: ServiceInput) => {
+    ['update-service', filter.value],
+    async ({ name, description, price, duration }: ServiceInput) => {
       const { data, error } = await supabase
         .from('services')
-        .insert([{ company_id, name, description, price, duration }])
+        .update({ name, description, price, duration })
+        .eq(filter?.column, filter.value)
         .select()
         .maybeSingle();
 

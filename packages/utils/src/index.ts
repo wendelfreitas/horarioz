5b14d3b13ffe10103e97124890316d0081e53422
Wendel Freitas <wendelfreitasbranco@gmail.com>
2023-07-05 17:41:11 -0300

    feat: create format duration to string function

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,5 @@
 export * from './helpers/get-currency-in-float-number/get-currency-in-float-number';
+export * from './helpers/get-number-in-currency-format/get-number-in-currency-format';
+export * from './helpers/format-duration-to-string/format-duration-to-string';
 
 export * from './constants';
